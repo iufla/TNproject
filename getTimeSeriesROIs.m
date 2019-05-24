613516800fb7201e17557b34a8bf3e1a94950880
alexjhess <alhess@student.ethz.ch>
2019-05-24 11:24:49 +0200

    updated ROI coordinates

--- getTimeSeriesROIs.m
+++ getTimeSeriesROIs.m
@@ -3,17 +3,17 @@ regions = struct();
 % specify ROI coordinates as [x,y,z] arrays
 % size is radius of sphere around [x,y,z] centre coordinates
 regions(1).name = 'MPFC';
-regions(1).coordinates = [-6, 54, 32];
-regions(1).size = 8;
+regions(1).coordinates = [4.5, 49, 27.5];
+regions(1).size = 9;
 regions(end+1).name = 'LTPJ';
-regions(end).coordinates = [-52, -64, 20];
-regions(end).size = 7.8;    % largest possible value such that still inside
+regions(end).coordinates = [-51, -56, 24];
+regions(end).size = 9;    % largest possible value such that still inside
 regions(end+1).name = 'RTPJ';
-regions(end).coordinates = [56, -46, 20];
-regions(end).size = 8;
+regions(end).coordinates = [54, -54, 23];
+regions(end).size = 9;
 regions(end+1).name = 'PREC';
-regions(end).coordinates = [2, -56, 40];
-regions(end).size = 8;
+regions(end).coordinates = [0.5, -56, 34];
+regions(end).size = 9;
 
 spm('defaults','FMRI')
 
