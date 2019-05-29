2d3b7745c8587351b0a7baac1c05101fd5828ced
Stephan Boner <stephan.boner@bluewin.ch>
2019-05-29 17:24:12 +0200

    deleted old files

--- DCM_input_spec_batch.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'C:\Users\Alexander\Documents\Studium\Master HST\Neuroscience\Spring Semester 2019\Translational Neuromodeling\Project\200_github repository\TNproject\data\sub-04\GLM\sub-04_task-dis_run-01_bold\DCM_input_spec_batch_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
